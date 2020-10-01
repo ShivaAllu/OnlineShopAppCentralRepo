323beaa5fb8f1721ad5879ab2b7cf3ac171e8c82
shivaAllu <shiva.a1995@gmail.com>
2020-10-01 14:41:56 +0530

    Committinig schanges.

--- OnlineShopApp/OnlineShopApp/src/main/java/com/cg/onlineshop/controllers/ProductController.java
+++ OnlineShopApp/OnlineShopApp/src/main/java/com/cg/onlineshop/controllers/ProductController.java
@@ -1,9 +1,10 @@
 package com.cg.onlineshop.controllers;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cg.onlineshop.beans.Product;
+import org.springframework.http.HttpStatus;
 @RestController
 public class ProductController {
 
